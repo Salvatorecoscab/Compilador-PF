00e14e206bbba8982236c0ba043331aea903a3e3
El chavacaba <salvadorcoscab@gmail.com>
2023-06-19 13:53:00 -0600

    Se conecta html con index js a traves de post

---
+++ index.js
@@ -0,0 +1,23 @@
+
+document.getElementById('VerificaCodigo').addEventListener('submit', function(e) {
+    e.preventDefault();  // prevent the form from submitting normally
+
+    var formData = new FormData(e.target);
+    
+    // Get the first name from the form data
+    var codigo = formData.get('codigo');
+    console.log(codigo)
+    // Check if first name is "Salvador"
+    if (codigo === 'Salvador') {    
+        //write in the text label the message
+        document.getElementById('resultlabel').innerHTML = 'Codigo Correcto';
+    }else 
+    {
+        document.getElementById('resultlabel').innerHTML = 'Codigo Incorrecto';
+    }
+
+    // Clear the form
+    document.getElementById('VerificaCodigo').reset();
+
+
+});
